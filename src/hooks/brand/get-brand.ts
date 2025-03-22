bd2d78c14aa8052e465681ac98c569ad62f06b51
Shaxriyor <shaxriyorziyodullayev816@gmail.com>
2025-03-22 09:18:20 +0500

    updated get brand

--- src/hooks/brand/get-brand.ts
+++ src/hooks/brand/get-brand.ts
@@ -2,13 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 import request from "@/services";
 import { Brand, IPopularBrand, PageInterface } from "@/types";
 
-export const useGetBrand = (popular?: boolean ) => {
+export const useGetBrand = (popular?: boolean) => {
   return useQuery<Brand[] | IPopularBrand[], Error>({
     queryKey: ["brand", popular],
     queryFn: async () => {
-      const res = await request.get<PageInterface<Brand[] | IPopularBrand[]>>(
-        popular ? `/brand/all?popular=${popular}` : "/brand/all"
-      );
+      let url = "/brand/all";
+
+      if (popular !== undefined) {
+        url += `?popular=${popular}`;
+      }
+
+      const res = await request.get<PageInterface<Brand[] | IPopularBrand[]>>(url);
       return res.data.data;
     },
   });
